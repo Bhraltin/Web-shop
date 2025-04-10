28110bcd30469f41079e4bf0049dfbc64f331d72
Bhraltin <bhriskts10@gmail.com>
2025-04-10 14:16:02 +0300

    tailwind config prepared

--- Web-shop/src/components/layout/header.jsx
+++ Web-shop/src/components/layout/header.jsx
@@ -1,7 +1,15 @@
-export default function Header () {
-    return(
-        <header className="w-full">
 
-        </header>
-    )
+
+
+export default function Header() {
+  return (
+<header className="w-full bg-background shadow-md">
+      <div className="flex flex-row items-center justify-between px-4 py-3 max-w-7xl mx-auto">
+        <h1 className="text-2xl font-bold text-primary">
+          Web Shop
+        </h1>
+ 
+      </div>
+    </header>
+  )
 }
