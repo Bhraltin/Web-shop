667677053df1f8d34ff294871b0e8e9632fed918
Bhraltin <bhriskts10@gmail.com>
2025-04-13 17:45:31 +0300

    background fixed

---
+++ Web-shop/src/components/background.jsx
@@ -0,0 +1,10 @@
+export default function Background() {
+    return (
+      <div className="fixed inset-0 z-0">
+        <div className="absolute -top-40 -left-40 w-80 h-80 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+        <div className="absolute top-0 -right-20 w-80 h-80 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+        <div className="absolute -bottom-40 left-20 w-80 h-80 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+      </div>
+    )
+  }
+  
--- Web-shop/src/pages/LandingPage.jsx
+++ Web-shop/src/pages/LandingPage.jsx
@@ -1,8 +1,10 @@
+import Background from "../components/background"
 import Layout from "../components/layout/Layout"
 export default function LandingPage() {
     return (
       <>
       <Layout>
+        <Background/>
         <div>LandingPage</div>
         </Layout>
         </>
